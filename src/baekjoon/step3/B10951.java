34efde7a26348c09dac5c37736bc40433610a7fb
seojw0730 <seojw0730@naver.com>
2024-12-06 01:51:09 +0900

    3단계

---
+++ src/baekjoon/step3/B10951.java
@@ -0,0 +1,21 @@
+package baekjoon.step3;
+
+import java.util.*;
+import java.io.*;
+
+public class B10951 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		while(true) {
+			String input1 = br.readLine();
+			if(input1 == null) break;
+			String[] arr = input1.split(" ");
+			int a = Integer.parseInt(arr[0]);
+			int b = Integer.parseInt(arr[1]);
+			bw.append((a + b) + "\n");
+		}
+		br.close();
+		bw.close();
+	}
+}
---
+++ src/baekjoon/step3/B11022.java
@@ -0,0 +1,20 @@
+package baekjoon.step3;
+
+import java.util.*;
+import java.io.*;
+
+public class B11022 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int t = Integer.parseInt(br.readLine());
+		for(int i = 1; i <= t; i++) {
+			String[] input = br.readLine().split(" ");
+			int a = Integer.parseInt(input[0]);
+			int b = Integer.parseInt(input[1]);
+			bw.append("Case #" + i + ": " + a + " + " + b + " = " + (a + b) + "\n");
+		}
+		br.close();
+		bw.close();
+	}
+}

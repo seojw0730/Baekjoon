15615beed3491dd9ab89edceaf703634349beb32
seojw0730 <seojw0730@naver.com>
2024-12-07 01:53:22 +0900

    5단계

---
+++ src/baekjoon/step5/B2743.java
@@ -0,0 +1,14 @@
+package baekjoon.step5;
+
+import java.util.*;
+import java.io.*;
+
+public class B2743 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String input1 = br.readLine();
+		br.close();
+		bw.append(String.valueOf(input1.length())).close();
+	}
+}
